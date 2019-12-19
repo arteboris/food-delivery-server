b72b9d6a94f508befd7b52537a9a46b8030adc18
arteboris <arteboris@ukr.net>
2019-12-19 23:59:19 +0200

    done homework 1

--- demo/src/routes/router.js
+++ demo/src/routes/router.js
@@ -1,8 +1,10 @@
 const mainRoute = require('./main/main');
 const carRoute = require('./car/car');
 const productsRoute = require('./products/products');
+const signUpRoute = require('./users/signup');
 
 const router = {
+    '/signup': signUpRoute,
     '/car': carRoute,
     '/products': productsRoute,
     default: mainRoute,
